7e73b2dcd387572caea0d8edf68f1f183b88483c
IrynaPisaryk <Irina_Pisarik@epam.com>
2015-02-06 18:04:14 +0300

    modified main class

--- src/main/java/client/FTPClientMav.java
+++ src/main/java/client/FTPClientMav.java
@@ -1,6 +1,5 @@
 package client;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -13,7 +12,6 @@ import org.apache.commons.net.ftp.FTPConnectionClosedException;
 import org.apache.commons.net.ftp.FTPFile;
 
 public class FTPClientMav {
-
 	private static FTPClientMav clientObj;
 	private FTPClientMav(){         
 	}
@@ -25,23 +23,23 @@ public class FTPClientMav {
 	}
 	public static void main(String[] args) throws MalformedURLException, IOException{
 		FTPClientMav newClient = FTPClientMav.getInstance();
-		newClient.StartFTPClient();
+		newClient.startFTPClient();
 	}
-	public void StartFTPClient() throws MalformedURLException, IOException{
+	public void startFTPClient() throws MalformedURLException, IOException{
 		boolean ftpError = false;
 		try{
-			FTPClient client = FTPconnect();
-			PrintListFile(client, ftpError);
-			FTPdisconnect(client, ftpError);
+			FTPClient client = ftpConnect();
+			printListFile(client, ftpError);
+			ftpDisconnect(client, ftpError);
 		}catch(FTPConnectionClosedException e){
 			ftpError = true;
 		}
-		if(ftpError == true){
+		if(ftpError){
 			System.out.println("Error. FTP connection is closed. Try to restart program");
 		}
-			
+
 	}
-	public static FTPClient FTPconnect() throws MalformedURLException, IOException{
+	public static FTPClient ftpConnect() throws MalformedURLException, IOException{
 		FTPClient client = new FTPClient();
 		client.connect("ftp.mozilla.org");
 		client.login("anonymous", "anonymous");
@@ -49,7 +47,7 @@ public class FTPClientMav {
 		client.setFileType(FTP.BINARY_FILE_TYPE);
 		return client;
 	}
-	public static void FTPdisconnect(FTPClient client, boolean ftpError) throws IOException{
+	public static void ftpDisconnect(FTPClient client, boolean ftpError) throws IOException{
 		try{
 			client.logout();
 			client.disconnect();
@@ -57,14 +55,14 @@ public class FTPClientMav {
 			ftpError = true;
 		}
 	}
-	public static void UserDialog(){
+	public static void userDialog(){
 		System.out.println("What you want to do?");
 		System.out.println("1. Download file(1)");
 		System.out.println("2. Change current directory(2)");
 		System.out.println("3. Change to parent directory(3)");
 		System.out.println("4. Exit(4)");
 	}
-	public static void PrintListFile(FTPClient client, boolean ftpError) throws IOException{
+	public static void printListFile(FTPClient client, boolean ftpError) throws IOException{
 
 		boolean stopProgram = false;
 		do{
@@ -75,7 +73,7 @@ public class FTPClientMav {
 			}
 			int fileCount = 0;
 			for(int i = 0; i < ftpFiles.length; i++){
-				if(ftpFiles[i].isFile()==true)
+				if(ftpFiles[i].isFile())
 					fileCount++;				
 			}		
 			String action = null;
@@ -90,32 +88,32 @@ public class FTPClientMav {
 				int actionInt = Integer.parseInt(action);				
 				switch(actionInt){
 				case(1):
-					FileDownload(client, ftpError);
+					fileDownload(client, ftpError);
 				break;
 				case(2):
-					ChangeToParentDirectory(client, ftpError);
+					changeToParentDirectory(client, ftpError);
 				break;
 				case(3):
 					stopProgram = true;
-					System.out.println("Completed");				
+				System.out.println("Completed");				
 				break;
 				}
 			}
 			else{
-				UserDialog();		
+				userDialog();		
 				action = scan.nextLine();
 				try{
 
 					int actionInt = Integer.parseInt(action);
 					switch(actionInt){
 					case(1):
-						FileDownload(client, ftpError);
+						fileDownload(client, ftpError);
 					break;
 					case(2):				
-						ChangeCurrentDirectory(client, ftpFiles, scan, ftpError);
+						changeCurrentDirectory(client, ftpFiles, scan, ftpError);
 					break;
 					case(3):
-						ChangeToParentDirectory(client, ftpError);
+						changeToParentDirectory(client, ftpError);
 					break;
 					case(4):
 						System.out.println("Completed");
@@ -126,29 +124,29 @@ public class FTPClientMav {
 						break;
 
 					}	
-				}catch(NumberFormatException e){
+				} catch (NumberFormatException e) {
 					System.out.println("Incorrect symbol");
-				}catch(FTPConnectionClosedException e){
+				} catch (FTPConnectionClosedException e) {
 					ftpError = true;
-				}catch(NoSuchElementException e){
+				} catch (NoSuchElementException e) {
 					e.printStackTrace();
 				}
 			}
 
 		}while(stopProgram == false);
 	}
-	public static void ChangeToParentDirectory(FTPClient client, boolean ftpError) throws IOException{
-		try{
-			if(client.printWorkingDirectory().equals("/")==true){
+	public static void changeToParentDirectory(FTPClient client, boolean ftpError) throws IOException{
+		try {
+			if (client.printWorkingDirectory().equals("/")) {
 				System.out.println("This is root!");
-			}else{
+			} else {
 				client.changeToParentDirectory();
 			}
-		}catch(FTPConnectionClosedException e){
+		} catch (FTPConnectionClosedException e) {
 			ftpError = true;
 		}
 	}
-	public static void ChangeCurrentDirectory(FTPClient client, FTPFile[] ftpFiles, Scanner scan, boolean ftpError) throws IOException{
+	public static void changeCurrentDirectory(FTPClient client, FTPFile[] ftpFiles, Scanner scan, boolean ftpError) throws IOException{
 		try{					
 			String newDir = null;
 			System.out.println("Enter directory name");
@@ -166,7 +164,7 @@ public class FTPClientMav {
 			ftpError = true;
 		}
 	}
-	public static void FileDownload(FTPClient client, boolean ftpError) throws IOException{
+	public static void fileDownload(FTPClient client, boolean ftpError) throws IOException{
 		try{	
 
 			String filename = null;
@@ -182,16 +180,19 @@ public class FTPClientMav {
 			}
 			System.out.println("Enter the path to destination folder");
 			path = scan.nextLine();	
-			boolean canWrite = CanWrite(path);
-			if(canWrite == true && fileExist == true){				
+			if(fileExist){				
 				FileOutputStream fos = null;
-				try {				    
-					fos = new FileOutputStream(path+"\\"+filename);
-					client.retrieveFile(filename, fos);
-					System.out.println("File "+filename+" has been downloaded successfully.");
+				try {
+					try{
+						fos = new FileOutputStream(path+"\\"+filename);
+						client.retrieveFile(filename, fos);
+						System.out.println("File "+filename+" has been downloaded successfully.");
+					}catch(FileNotFoundException e){
+						System.out.println("Bad path. Try again");
+					}					
 				} catch (IOException e) {
 					e.printStackTrace();
-				} finally {
+				}finally {
 					try {
 						if (fos != null) {
 							fos.close();
@@ -212,22 +213,6 @@ public class FTPClientMav {
 		catch(FTPConnectionClosedException e){
 			ftpError = true;
 		}
-	}
-	public static boolean CanWrite(String path){
-		int DiskCounter = 0;			
-		for (File f:File.listRoots()) {
-			if(path.toUpperCase().startsWith(f.getPath())== true && f.canWrite() == true){
-				return true;			
-			}
-			else{
-				DiskCounter++;
-			}
-		}
-		if(DiskCounter !=0){
-			System.out.println("This path doesn't exists or blocked");
-			return false;
-		}
-		return false;
 	}
 }
 
